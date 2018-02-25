3fa84463b2a4b6884ff66bbd345df08d6d561de0
adadesions <adadesions@gmail.com>
2018-02-26 02:01:47 +0700

    Remove machanism app

--- training_site/mechanism/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from mechanism.models import UploadImage
-
-
-class UploadImageAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(UploadImage, UploadImageAdmin)
--- training_site/mechanism/forms.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.forms import ModelForm
-from .models import UploadImage
-
-
-class UploadFileForm(ModelForm):
-    class Meta:
-        model = UploadImage
-        fields = ['file', 'filename']
